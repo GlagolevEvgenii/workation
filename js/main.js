745442352aa7d5c3dcc3b269d8f4b99913e5aa68
IevgeniiGlagoliev <glagolev.dev@gmail.com>
2024-02-29 23:36:26 +0200

    change files

--- js/main.js
+++ js/main.js
@@ -1,51 +1,11 @@
-function checkWebpFeature(feature, callback) {
-    var kTestImages = {
-        lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
-        lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
-        alpha:
-            "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
-        animation:
-            "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA",
-    };
-    var img = new Image();
-    img.onload = function () {
-        var result = img.width > 0 && img.height > 0;
-        callback(feature, result);
-    };
-    img.onerror = function () {
-        callback(feature, false);
-    };
-    img.src = "data:image/webp;base64," + kTestImages[feature];
-}
-
-function isWebp(feature, result) {
-    document.body.classList.add(result ? "webP" : "noWebp");
-}
-
-checkWebpFeature("lossy", isWebp);
 const embedEngine = {
     init() {
         embedEngine.binds();
     },
     binds() {
-        const swiper = new Swiper('.swiper', {
-            slidesPerView: 1,
-            pagination: {
-                el: '.swiper-pagination',
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            breakpoints: {
-                320: {
-                    slidesPerView: 1,
-                },
-                767: {
-                    slidesPerView: 2.2,
-                    spaceBetween: 20
-                }
-            }
+
+        document.querySelectorAll(".embed-button").forEach((e) => {
+            e.addEventListener("click", embedEngine.embedBox);
         });
 
         const menuBtnRef = document.querySelector("[data-menu-button]");
@@ -68,39 +28,11 @@ const embedEngine = {
             document.body.classList.toggle("is-open");
         });
 
-        window.onscroll = throttle(scrollFunction, 500);
-
-        function throttle(func, ms) {
-            let isThrottled = false,
-                savedArgs,
-                savedThis;
-
-            function wrapper() {
-                if (isThrottled) {
-                    // (2)
-                    savedArgs = arguments;
-                    savedThis = this;
-                    return;
-                }
-
-                func.apply(this, arguments); // (1)
-
-                isThrottled = true;
-
-                setTimeout(function () {
-                    isThrottled = false; // (3)
-                    if (savedArgs) {
-                        wrapper.apply(savedThis, savedArgs);
-                        savedArgs = savedThis = null;
-                    }
-                }, ms);
-            }
-
-            return wrapper;
-        }
+        window.onscroll = function () {
+            scrollFunction();
+        };
 
         function scrollFunction() {
-
             if (
                 document.body.scrollTop > 466 ||
                 document.documentElement.scrollTop > 466
