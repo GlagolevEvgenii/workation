b88dbf7d74afdd0585420a19481a418a4f03656a
IevgeniiGlagoliev <glagolev.dev@gmail.com>
2024-03-05 12:24:01 +0200

    fix bugs, add button up

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const embedEngine = {
         embedEngine.binds();
     },
     binds() {
-
+        let scrollToTopBtn = document.querySelector(".scrollup");
         document.querySelectorAll(".embed-button").forEach((e) => {
             e.addEventListener("click", embedEngine.embedBox);
         });
@@ -38,10 +38,21 @@ const embedEngine = {
                 document.documentElement.scrollTop > 466
             ) {
                 document.querySelector(".nav").classList.add("nav--sticky");
+                scrollToTopBtn.classList.add("showBtn");
             } else {
                 document.querySelector(".nav").classList.remove("nav--sticky");
+                scrollToTopBtn.classList.remove("showBtn");
             }
         }
+        function scrollToTop() {
+            document.querySelector(".hero").scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+        scrollToTopBtn.addEventListener("click", scrollToTop);
     },
 };
 document.addEventListener("DOMContentLoaded", embedEngine.init);
